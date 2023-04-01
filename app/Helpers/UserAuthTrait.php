90a63c290ea5e8ff1ccb26a71bdeeab000085d85
Aleh Zahorski <git3371m@gmail.com>
2023-04-01 23:34:07 +0200

    some changes include user trait

--- app/Helpers/UserAuthTrait.php
+++ app/Helpers/UserAuthTrait.php
@@ -2,18 +2,31 @@
 
 namespace App\Helpers;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
 
 trait UserAuthTrait
 {
     public function isUserAuth(): bool
     {
-        $user =  Auth::guard('sanctum')->user();
-
-        if (!$user) {
+        if (!$this->getUser()) {
             return false;
         }
 
         return true;
+    }
+
+    public function getUserId(): ?int
+    {
+        if (!$this->getUser()) {
+            return null;
+        }
+
+        return $this->getUser()->getAuthIdentifier();
+    }
+
+    private function getUser(): Authenticatable
+    {
+        return Auth::guard('sanctum')->user();
     }
 }
